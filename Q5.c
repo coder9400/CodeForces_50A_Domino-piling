dc446740f12f7169675c049fbd23c060fec45aba
coder9400 <coderv9600@gmail.com>
2025-04-08 11:10:47 +0530

    Add files via upload

---
+++ Q5.c
@@ -0,0 +1,12 @@
+// Domino piling
+#include<stdio.h>
+int main()
+{
+    int N,M;
+    printf("Enter length of board : ");
+    scanf("%d",&M);
+    printf("Enter breadth of board : ");
+    scanf("%d",&N);
+    printf("Output : %d",(M*N)/2);
+    return 0;
+}
